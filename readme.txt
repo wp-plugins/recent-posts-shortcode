9c5fe2b83363ea70abddd58c967670b4076f3867
philhudson91 <philhudson91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-09 16:36:04 +0000

    tagging version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-shortcode/tags/0.1@1195566 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,7 @@
+A simple, open-source plugin to return your most recent posts via a shortcode.
+
+Author: Phil Hudson (http://www.phil-hudson.com)
+
+Usage:
+
+[recentPosts amountOfPosts="2"]
---
+++ recent-posts-shortcode.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Recent Posts Shortcode
+ * Plugin URI: http://www.phil-hudson.com/recent-posts-shortcode-wordpress-plugin/
+ * Description: A simple, open-source plugin to return your most recent posts via a shortcode.
+ * Version: 0.1
+ * Author: Phil Hudson
+ * Author URI: http://www.phil-hudson.com
+ * License: GPL12
+ */
+
+
+
+function recentPosts($atts){
+
+    $params = shortcode_atts( array(
+        'amountOfPosts' => 1,
+    ), $atts );
+
+    $getpost = get_posts( array('number' => $params['amountOfPosts']) );
+
+    $return = '';
+
+    foreach ($getpost as $post) {
+
+        $return .= "<div id=\"". $post->ID ."\"><h1 class=\"entry-title\">" . $post->post_title . "</h1><div class=\"entry-content\">" . $post->post_excerpt . "</div>";
+
+        $return .= "<br /><a href='" . get_permalink($post->ID) . "'><em>read more →</em></a></div></br>";
+
+    }
+
+    return $return;
+
+}
+add_shortcode('recentPosts', 'recentPosts');
